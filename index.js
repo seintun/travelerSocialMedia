0d864a2de622cfed0404c1df9c0fcb38bada53f2
seintun <seintun.edu@gmail.com>
2018-08-29 00:51:22 -0700

    clean js files, functions and added LOGIN features with local storage validation

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+// Store AvartarName and Password in Local Storage for later use
+function store() {
+    var inputName = document.querySelector("#avartarName");
+    var inputPassword = document.querySelector("#inputPassword1");
+
+    window.localStorage.setItem("avartarNameLS", inputName.value);
+    window.localStorage.setItem("inputPasswordLS", inputPassword.value);
+    window.onload = load;
+}
+
+// Upon Page loads or refresh, personalize greeting with stored name
 function load() {
-    console.log("load event detected!");
-    var name = window.localStorage.getItem('avartarName');
+    console.log("Page load detected!");
+    var name = window.localStorage.getItem('avartarNameLS');
       if (name !== null) {
         document.querySelector('.namePlaceholder').innerHTML = name;
       } else {
-          document.querySelector('.namePlaceholder').innerHTML = "Stranger";
+          document.querySelector('.namePlaceholder').innerHTML = "Traveler!";
       }
   }
-  window.onload = load;
+  window.onload = load();
+
+//   Valiate Login Inputs with stored Local Storage using querySelector
+function checkLocal() {
+    var inputLoginName = document.querySelector("#loginAvartar");
+    var inputLoginPassword = document.querySelector("#loginPassword");
+
+    if (inputLoginName.value === window.localStorage.getItem('avartarNameLS') && 
+    inputLoginPassword.value === window.localStorage.getItem('inputPasswordLS')) {
+            alert("Success!");
+            load();
+        } else {
+            alert("Invalid Avartar Name and/or Password. Try Again!");
+        }
+}
